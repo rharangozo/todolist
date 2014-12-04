e71448a8bbcc7c938b59c09740427f06d1a417d0
rolandharangozo <roleee3@gmail.com>
2014-12-04 16:52:06 +0100

    Show tasks on page load

--- sb/src/main/java/rh/web/TaskDeskController.java
+++ sb/src/main/java/rh/web/TaskDeskController.java
@@ -1,13 +1,21 @@
 package rh.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import rh.persistence.TaskEntityDAO;
 
 @Controller
 public class TaskDeskController {
 
+    @Autowired
+    private TaskEntityDAO taskEntityDAO;
+
     @RequestMapping("/")
-    String getTaskDesk() {
-        return "desk";
+    public ModelAndView getTaskDesk() {
+        ModelAndView modelAndView = new ModelAndView("desk");
+        modelAndView.addObject("tasks", taskEntityDAO.list());
+        return modelAndView;
     }
 }
--- sb/src/main/webapp/WEB-INF/jsp/desk.jsp
+++ sb/src/main/webapp/WEB-INF/jsp/desk.jsp
@@ -1,6 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <%@ page language="java" contentType="text/html; charset=US-ASCII"
          pageEncoding="US-ASCII"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 
 <html>
     <head>
@@ -10,5 +11,12 @@
 
     <body>
         <h3>Task desk</h3><br>
+
+        <c:forEach items="${tasks}" var="task">
+            <c:out value="${task.id}"/>
+            <c:out value="${task.description}"/>
+            </br>
+        </c:forEach>
+
     </body>
 </html>
